a6149ab1f7c5b3b70df428620714499924e0758f
Patrick McClory <pmcclory@gmail.com>
2013-03-19 14:49:28 -0400

    wiping my machine
    checkpoint... cleaned w/o vars

---
+++ harParser/harParser/Program.cs
@@ -0,0 +1,171 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Google.GData.Client;
+using Google.GData.Extensions;
+using Google.GData.Spreadsheets;
+using Newtonsoft.Json;
+
+namespace harParser
+{
+    class Program
+    {
+        private static string buildEntry(string entry)
+        {
+            if (string.IsNullOrWhiteSpace(entry))
+            {
+                entry = "-1";
+            }
+            return @"""" + entry +@""",";
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Input google username:");
+            string userName = Console.ReadLine();
+            Console.WriteLine("Input google password:");
+            string password = Console.ReadLine();
+            Console.WriteLine("Input spreadsheet to enumerate");
+            string spreadsheetName = string.Empty;// Console.ReadLine();
+            Console.WriteLine("Sheet to use");
+            string sheetName = string.Empty;// Console.ReadLine();
+
+            string csvString = string.Empty;
+
+            if (string.IsNullOrEmpty(spreadsheetName))
+            {
+                spreadsheetName = "";
+            }
+
+            if (string.IsNullOrWhiteSpace(sheetName))
+            {
+                sheetName = "Sheet1";
+            }
+            
+            SpreadsheetsService service = new SpreadsheetsService("rightscale.com");
+            service.setUserCredentials(userName, password);
+
+            SpreadsheetQuery query = new SpreadsheetQuery();
+            SpreadsheetFeed feed = service.Query(query);
+
+            Console.WriteLine("Spreadsheets:");
+
+            AtomLink spreadSheetLink = null;
+
+            foreach (SpreadsheetEntry se in feed.Entries)
+            {
+                Console.WriteLine("  " + se.Title.Text);
+                if (se.Title.Text == spreadsheetName)
+                {
+                    spreadSheetLink = se.Links.FindService(GDataSpreadsheetsNameTable.WorksheetRel, null);
+                }
+            }
+
+            if (spreadSheetLink != null)
+            {
+                WorksheetQuery wsQuery = new WorksheetQuery(spreadSheetLink.HRef.ToString());
+                WorksheetFeed wsFeed = service.Query(wsQuery);
+
+                AtomLink cellFeedLink = null;
+
+                foreach (WorksheetEntry we in wsFeed.Entries)
+                {
+                    Console.WriteLine(" --" + we.Title.Text);
+                    if (we.Title.Text == sheetName)
+                    {
+                        cellFeedLink = we.Links.FindService(GDataSpreadsheetsNameTable.CellRel, null);
+                    }
+                }
+
+                if (cellFeedLink != null)
+                {
+                    CellQuery cellQuery = new CellQuery(cellFeedLink.HRef.ToString());
+                    CellFeed cellFeed = service.Query(cellQuery);
+
+                    uint currRow = 2;
+
+                    Guid rowID = Guid.NewGuid();
+                    csvString += buildEntry(rowID.ToString() );
+
+                    foreach (CellEntry curCell in cellFeed.Entries)
+                    {
+                        if (curCell.Row > 1)
+                        {
+                            if (curCell.Row != currRow)
+                            {
+                                rowID = Guid.NewGuid();
+                                csvString += Environment.NewLine + buildEntry(rowID.ToString() );
+                                currRow = curCell.Row;
+                            }
+                            if (curCell.Cell.Column != 6)
+                            {
+                                csvString += buildEntry(curCell.Value );
+                            }
+                            else
+                            {
+                                using(System.IO.StreamWriter sw = new System.IO.StreamWriter(@"D:\dev\harfiles\" + rowID.ToString() + ".har"))
+                                {
+                                    sw.Write(curCell.Cell.Value);
+                                    sw.Flush();
+                                }
+                            }
+                        }
+                    }
+                    if (!string.IsNullOrWhiteSpace(csvString))
+                    {
+                        using (System.IO.StreamWriter sw = new System.IO.StreamWriter(@"D:\dev\harfiles\_harHeaders.csv"))
+                        {
+                            sw.Write(csvString);
+                            sw.Flush();
+                        }
+                    }
+                }
+            }
+            string[] files = System.IO.Directory.GetFiles(@"D:\dev\harfiles", "*.har");
+            if(files != null && files.Length > 0)
+            {
+                foreach(string f in files)
+                {
+                    string testID = f.Replace(".har", "").Replace(@"D:\dev\harfiles\", "");
+                    HarFile hf = null;
+                    using (System.IO.StreamReader sr = new System.IO.StreamReader(f))
+                    {
+                        string contents = sr.ReadToEnd();
+                        hf = JsonConvert.DeserializeObject<HarFile>(contents);
+                    }
+                    string dataFileContents = string.Empty;
+                    foreach (var entries in hf.log.entries)
+                    {
+                        string dataline = string.Empty;
+                        dataline += buildEntry(entries.request.url );
+                        dataline += buildEntry(entries.request.method );
+                        dataline += buildEntry(entries.request.headersSize.ToString() );
+                        dataline += buildEntry(entries.request.bodySize.ToString() );
+                        dataline += buildEntry(entries.time.ToString() );
+                        dataline += buildEntry(entries.timings.dns.ToString() );
+                        dataline += buildEntry(entries.timings.connect.ToString() );
+                        dataline += buildEntry(entries.timings.send.ToString());
+                        dataline += buildEntry(entries.timings.blocked.ToString());
+                        dataline += buildEntry(entries.timings.wait.ToString() );
+                        dataline += buildEntry(entries.timings.ssl.ToString() );
+                        dataline += buildEntry(entries.timings.receive.ToString() );
+                    
+                        dataline += buildEntry(entries.response.bodySize.ToString());
+                        dataline += buildEntry(entries.response.status.ToString() );
+                        dataline += buildEntry(entries.response.statusText );
+                        dataFileContents += dataline + Environment.NewLine;
+                    }
+
+                    using (System.IO.StreamWriter sw = new System.IO.StreamWriter(@"d:\dev\harfiles\_hardetail.csv"))
+                    {
+                        sw.Write(dataFileContents);
+                        sw.Flush();
+                    }
+                }
+            }
+        }
+    }
+}
+
---
+++ harParser/harParser/harFileStructure.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace harParser
+{
+    public class Creator
+    {
+        public string name { get; set; }
+        public string version { get; set; }
+    }
+
+    public class PageTimings
+    {
+        public int onContentLoad { get; set; }
+        public int onLoad { get; set; }
+    }
+
+    public class Page
+    {
+        public string startedDateTime { get; set; }
+        public string id { get; set; }
+        public string title { get; set; }
+        public PageTimings pageTimings { get; set; }
+    }
+
+    public class Param
+    {
+        public string name { get; set; }
+        public string value { get; set; }
+    }
+
+    public class PostData
+    {
+        public string mimeType { get; set; }
+        public string text { get; set; }
+        public List<Param> @params { get; set; }
+    }
+
+    public class Request
+    {
+        public string method { get; set; }
+        public string url { get; set; }
+        public string httpVersion { get; set; }
+        public List<object> headers { get; set; }
+        public List<object> queryString { get; set; }
+        public List<object> cookies { get; set; }
+        public int headersSize { get; set; }
+        public int bodySize { get; set; }
+        public PostData postData { get; set; }
+    }
+
+    public class Content
+    {
+        public int size { get; set; }
+        public string mimeType { get; set; }
+        public int compression { get; set; }
+    }
+
+    public class Response
+    {
+        public int status { get; set; }
+        public string statusText { get; set; }
+        public string httpVersion { get; set; }
+        public List<object> headers { get; set; }
+        public List<object> cookies { get; set; }
+        public Content content { get; set; }
+        public string redirectURL { get; set; }
+        public int headersSize { get; set; }
+        public int bodySize { get; set; }
+    }
+
+    public class Cache
+    {
+    }
+
+    public class Timings
+    {
+        public string blocked { get; set; }
+        public string dns { get; set; }
+        public string connect { get; set; }
+        public string send { get; set; }
+        public string wait { get; set; }
+        public string receive { get; set; }
+        public string ssl { get; set; }
+    }
+
+    public class Entry
+    {
+        public string startedDateTime { get; set; }
+        public int time { get; set; }
+        public Request request { get; set; }
+        public Response response { get; set; }
+        public Cache cache { get; set; }
+        public Timings timings { get; set; }
+        public string pageref { get; set; }
+    }
+
+    public class Log
+    {
+        public string version { get; set; }
+        public Creator creator { get; set; }
+        public List<Page> pages { get; set; }
+        public List<Entry> entries { get; set; }
+    }
+
+    public class HarFile
+    {
+        public Log log { get; set; }
+    }
+}
